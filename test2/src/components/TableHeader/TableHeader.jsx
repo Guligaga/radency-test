52c0804ebcc011a92b245c72c378ebe831b76a92
Evgeniy Koval <glggboko@gmail.com>
2021-07-04 03:14:01 +0300

    Edit table header

--- test2/src/components/TableHeader/TableHeader.jsx
+++ test2/src/components/TableHeader/TableHeader.jsx
@@ -2,30 +2,29 @@ import TableRow from '../TableRow/TableRow'
 
 // import { capitalCase } from "change-case";
 
-function TableHeader(props) {
-    const {parent} = props;
-
-    const alias = { 
+const HEADER_FIELDS = {
+    'notes-list': {
         name: 'Name',
         date: 'Date',
         category: 'Category',
         content: 'Content',
         datesList: 'Dates',
-        categoryName: 'Category',
+    },
+    'summary': {
+        category: 'Category',
         active: 'Active',
         archived: 'Archived',
-    };
-    const headers = Object.keys(props.data).reduce((acc, field) => {
-        if(alias.hasOwnProperty(field)) {
-            acc[field] = alias[field];
-        }
-        return acc
-    }, {})
+    }
+}
+
+function TableHeader({parent}) {
+
+    
     // console.log(headers);
     const actions = parent === 'notes-list' ? ['archivateAll', 'deleteAll'] : [];
     const type = parent === 'notes-list' ? 'header' : 'summary-header'
     return (
-        <TableRow data={headers} type={type} parent={parent} actions={actions}/>
+        <TableRow data={HEADER_FIELDS[parent]} type={type} parent={parent} actions={actions}/>
     );
 }
 
